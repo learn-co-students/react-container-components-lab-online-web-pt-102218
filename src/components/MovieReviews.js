a7b158ab27092483aa3738c8cb4b89a837c335fb
Arye Eidelman <aryedov.e@gmail.com>
2019-08-08 15:15:42 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export default function MovieReviews(props){
-	return <div className="review-list" style={{display: "flex", flexWrap: "wrap"}}>
+export default function MovieReviews(props) {
+	return <div className="review-list" style={{ display: "flex", flexWrap: "wrap" }}>
 
 		{props.reviews.map(review => {
 			let hasRating = ["R", "G", "PG", "PG-13"].includes(review.mpaa_rating)
 			let ratingIcon
-			if(hasRating){
+			if (hasRating) {
 				ratingIcon = (
 					review.mpaa_rating === "R" ? "https://upload.wikimedia.org/wikipedia/commons/7/7e/RATED_R.svg" :
 					review.mpaa_rating === "G" ? "https://upload.wikimedia.org/wikipedia/commons/0/05/RATED_G.svg" :
@@ -15,21 +15,24 @@ export default function MovieReviews(props){
 					""
 				)
 			}
+			let hasCoverPhoto = !!review.multimedia
+			let width = hasCoverPhoto ? review.multimedia.width * 2 : 420
+			let height = hasCoverPhoto ? review.multimedia.height * 4 : 560
 
-			return <a className="review" href={review.link.url} key={review.display_title} style={{ color: "black", textDecoration: "none"}}>
-				<div style={{width: review.multimedia.width * 2, height: review.multimedia.height * 4, margin: "1.25em", boxShadow: "black 5px 5px 20px -5px"}}>
-					<img src={review.multimedia.src} width={review.multimedia.width * 2} alt={`${review.display_title} - Cover Photo`}/>
-					<div style={{padding: "0 1.15em 1.15em"}}>
-						<div style={{display: "flex", width: "100%", justifyContent: "space-between"}}>
+			return <a className="review" href={review.link.url} key={review.display_title} style={{ color: "black", textDecoration: "none" }}>
+				<div style={{ width: `${width}px`, height: `${height}px`, margin: "1.25em", boxShadow: "black 5px 5px 20px -5px" }}>
+					{hasCoverPhoto && <img src={review.multimedia.src} width={`${width}px`} alt={`${review.display_title} - Cover Photo`} />}
+					<div style={{ padding: "0 1.15em 1.15em" }}>
+						<div style={{ display: "flex", width: "100%", justifyContent: "space-between" }}>
 							<h2>{review.display_title}</h2>
-							{hasRating && <img alt={`Rated ${review.mpaa_rating}`} width="75px" style={{transform: "rotate(10deg)", marginTop: "-2em"}} src={ratingIcon} />}
+							{hasRating && <img alt={`Rated ${review.mpaa_rating}`} width="75px" style={{ transform: "rotate(10deg)", marginTop: "-2em" }} src={ratingIcon} />}
 						</div>
 						<h3>{review.headline}</h3>
 						<p>{review.summary_short}</p>
 					</div>
 				</div>
 			</a>
-	
+
 		})}
 	</div>
 }
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -4,7 +4,47 @@ import MovieReviews from './MovieReviews'
 import secrets from '../secrets.json'
 
 const NYT_API_KEY = secrets.apiKeys.NYTimes;
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+const BASE_SEARCH_URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json'
 
-// Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      reviews: [],
+      searchTerm: "",
+    }
+  }
+
+  url = () => `${BASE_SEARCH_URL}?query=${this.state.searchTerm}&api-key=${NYT_API_KEY}`
+
+  handleSubmit = e => {
+    e.preventDefault()
+    fetch(this.url())
+      .then(resp => resp.json())
+      .then(json => {
+        this.setState({
+          ...this.state,
+          reviews: json.results || [],
+        })
+      })
+  }
+
+  handleFieldChange = e => {
+    this.setState({
+      ...this.state,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div className="searchable-movie-reviews" >
+        <form onSubmit={this.handleSubmit} >
+          <input type="text" name="searchTerm" onChange={this.handleFieldChange} value={this.state.searchTerm} />
+          <input type="submit" />
+        </form>
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    )
+  }
+}
