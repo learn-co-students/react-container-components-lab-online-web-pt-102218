d77b3a73a3f3e5e78c156fda2ea0400203edbc5d
Arye Eidelman <aryedov.e@gmail.com>
2019-08-08 00:36:29 -0400

    Create MovieReveiws card style

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,49 @@
-// Code MovieReviews Here
+import React from 'react';
+
+export default function MovieReviews(props){
+	return <div style={{display: "flex", flexWrap: "wrap"}}>
+
+		{props.reviews.map(review =>
+			<a href={review.link.url} key={review.display_title} style={{
+					color: "black",
+					textDecoration: "none"
+				}}>
+				<div style={{
+						width: review.multimedia.width * 2,
+						height: review.multimedia.height * 4,
+						margin: "1.25em",
+						boxShadow: "black 5px 5px 20px -5px",
+					}}>
+
+					<img src={review.multimedia.src} width={review.multimedia.width * 2} alt={`${review.display_title} - Cover Photo`}/>
+
+					<div style={{padding: "0 1.15em 1.15em"}}>
+
+						<div style={{display: "flex", width: "100%", justifyContent: "space-between"}}>
+							<h2>{review.display_title}</h2>
+							{
+								["R", "G", "PG", "PG-13"].includes(review.mpaa_rating)
+									? <img alt={`Rated ${review.mpaa_rating}`}  width="75px" style={{transform: "rotate(10deg)", marginTop: "-2em"}}
+											src={
+												review.mpaa_rating === "R" ? "https://upload.wikimedia.org/wikipedia/commons/7/7e/RATED_R.svg" :
+												review.mpaa_rating === "G" ? "https://upload.wikimedia.org/wikipedia/commons/0/05/RATED_G.svg" :
+												review.mpaa_rating === "PG" ? "https://upload.wikimedia.org/wikipedia/commons/b/bc/RATED_PG.svg" :
+												review.mpaa_rating === "PG-13" ? "https://upload.wikimedia.org/wikipedia/commons/c/c0/RATED_PG-13.svg" :
+												""
+											}
+										/>
+								: <span></span>
+
+							}
+						</div>
+
+						<h3>{review.headline}</h3>
+
+						<p>{review.summary_short}</p>
+
+					</div>
+				</div>
+			</a>
+		)}
+	</div>
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import LatestMovieReviewsContainer from './components/LatestMovieReviewsContainer';
-import SearchableMovieReviewsContainer from './components/SearchableMovieReviewsContainer';
+// import LatestMovieReviewsContainer from './components/LatestMovieReviewsContainer';
+// import SearchableMovieReviewsContainer from './components/SearchableMovieReviewsContainer';
+import MovieReviews from './components/MovieReviews';
+import exampleApiResponse from './exampleApiResponse.json'
 
 ReactDOM.render(
   <div className="app">
-    <SearchableMovieReviewsContainer />
-    <LatestMovieReviewsContainer />
+    {/* <SearchableMovieReviewsContainer />
+    // <LatestMovieReviewsContainer /> */}
+    <MovieReviews reviews={exampleApiResponse.results} />
   </div>,
   document.getElementById('root')
 );
