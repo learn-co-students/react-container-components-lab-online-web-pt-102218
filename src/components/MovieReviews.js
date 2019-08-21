227ec4e4439d4fbf21134b6f8d7ec4883a9443b0
beaulieu527 <beaulieudev@gmail.com>
2019-08-21 11:46:18 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,18 @@
 // Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = (props) => 
+    <div className="review-list">
+        {props.reviews.map((review) => {
+            return (
+                <div className="review">
+                    <h1>Display Title: {review.display_title}</h1>
+                    <h2>Headline: {review.headline}</h2>
+                    <p>Summary Short: {review.summary_short}</p>
+                    <a href={review.link.url}>{review.link.suggested_link_text}</a>
+                </div>
+            )
+        })}
+    </div>
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,54 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
+const NYT_API_KEY = 'JDVlLCcAbGBqGMGxK0nGEQS2QSs5fdcs';
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=dogs&'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            reviews : [],
+            searchTerm : ''
+        }
+    }
+
+    handleChange(event){
+        this.setState({
+            ...this.state,
+            [event.target.name] : event.target.value
+        })
+    }
+
+    handleSubmit(event){
+        event.preventDefault()
+        fetch('https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=' + this.state.searchTerm + '&'+ `api-key=${NYT_API_KEY}`)
+            .then(function(response) {
+                if (response.status >= 400) {
+                    throw new Error("Bad response from server");
+                }
+                return response.json();
+            })
+            .then((myJson) => {
+                this.setState({
+                    reviews: myJson.results.slice(0, 3)
+                })
+            });
+    }
+
+    render(){
+        return(
+            <div className="searchable-movie-reviews">
+                <form onSubmit={(event) => this.handleSubmit(event)}>
+                    <input type="text" name="searchTerm" value={this.state.searchTerm} onChange={(event) => this.handleChange(event)}></input>
+                    <button type="submit">submit</button>
+                </form> 
+                <MovieReviews reviews={this.state.reviews}/>
+            </div>
+        )
+    }
+}
+
+export default SearchableMovieReviewsContainer
